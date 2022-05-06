ff776b702116e8de752560c43dd5aa04d37acc25
Fabian Weiand <fabian.weiand@snhu.edu>
2022-05-06 14:21:42 -0400

    clean up

--- main.py
+++ main.py
@@ -5,12 +5,7 @@ import re
 orig_path = input("Enter origin file path: ")
 while(not os.path.exists(orig_path)):
     orig_path = input("Invlaid path. Enter origin file path: ")
-#(r"C:\Users\Fabian\OneDrive\Documents\Projects\RyanDroneFileRename\30Apr2022")
-
 dest_path = input("Enter destination file path: ")
-while(not os.path.exists(dest_path)):
-    orig_path = input("Invlaid path. Enter destination file path: ")
-#(r"C:\Users\Fabian\OneDrive\Documents\Projects\RyanDroneFileRename\30Apr2022_R")
 count = 0
 
 # Create list of files at origin
